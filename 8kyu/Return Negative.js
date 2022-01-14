b1d3605404c8830c82c4363401752470c0457cec
Stephen Gallagher <sgallagher92@gmail.com>
2022-01-14 14:13:16 +0100

    Return Negative Kata

--- 8kyu/Return Negative.js
+++ 8kyu/Return Negative.js
@@ -1,19 +1,19 @@
 /*
-RETURN NEGATIVE
+  RETURN NEGATIVE
 
 https://www.codewars.com/kata/55685cd7ad70877c23000102/train/javascript
 
-*** Instructions ***
+--- Instructions ---
 In this simple assignment you are given a number and have 
 to make it negative. But maybe the number is already negative?
 
---- Notes ---
-The number can be negative already, in which case no change is required.
-Zero (0) is not checked for any specific sign. Negative zeros make no 
-mathematical sense.
+*** Notes ***
+- The number can be negative already, in which case no change is required.
+- Zero (0) is not checked for any specific sign. Negative zeros make no 
+  mathematical sense.
 */
 
-//SOLUTION 1
+// SOLUTION 1 --------
 function makeNegative(num) {
   if (num > 0) {
     return -num;
@@ -21,7 +21,7 @@ function makeNegative(num) {
   return num;
 }
 
-//SOLUTION 2
+// SOLUTION 2 --------
 function makeNegative(num) {
   if (num > 0) {
     return num * -1;
@@ -29,7 +29,7 @@ function makeNegative(num) {
   return num;
 }
 
-// SOLUTION 3
+// SOLUTION 3 --------
 function makeNegative(num) {
   return num < 0 ? num : -num;
 }
